fc0c287624dbdbea01950fa7809f358e115dd20f
wesleynecreto <wesleynecreto@gmail.com>
2026-09-09 09:51:16 -0300

    while

---
+++ logica/while_1.java
@@ -0,0 +1,10 @@
+public class while_1 {
+    public static void main(String[] args){
+        System.out.println("\n1. contar de 1 ate 5 com while\n");
+        int contador = 1;
+        while (contador <=5){
+            System.out.println("Contador: " + contador);
+            contador++;
+        }
+    }
+}
